ebbf7ff28173861dea0be2c413349df7900258d2
Davide Gozzi <94189785+DavideGozzi02@users.noreply.github.com>
2021-11-19 19:46:05 +0000

    finito

--- lezione3.py
+++ lezione3.py
@@ -1,10 +1,12 @@
 values = []
 my_file=open('shampoo_sales.csv', 'r')
 for line in my_file:
-    values=my_file.split(',')
-    mio_numero=float(values)
-    somma=somma+mio_numero
-
-print(somma)
-print(my_file.read())
+    elements=line.split(',')
+    values=line.split(',')
+    if (elements[0]!='Date'):
+        date=elements[0]
+        value=elements[1]
+sum(values)
+print(sum(values))
+#print(my_file.read())
 my_file.close 
