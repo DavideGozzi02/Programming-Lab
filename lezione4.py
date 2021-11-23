f69e7a3505638f9292152bb3ae3beb3c754187b3
Davide Gozzi <94189785+DavideGozzi02@users.noreply.github.com>
2021-11-23 16:53:15 +0000

    finale

--- lezione4.py
+++ lezione4.py
@@ -1,20 +1,20 @@
 class CSVFile():
-    def  __init__(self, file):
-        self.name=name
+    def  __init__(self, file_name):
+        self.name=file_name
 
     def get_data(self):
         file_list = []   
-        openedfile = open(self.file_name, 'r')
+        openedfile = open(self.name, 'r')
         for line in openedfile:
             elements = line.split(',')
-            if elements[0] != "Date"
+            if elements[0] != "Date":
                 lineElements = line.split (',')
-                lineElements [1] = lineElements [1].strip()
-            file_list.append(lineElements)
+                #lineElements [1] = lineElements [1].strip()
+                file_list.append(lineElements)
         return file_list 
 
 shampooFile = CSVFile ('shampoo_sales.csv')
-shampoo_sales = shampooFile.get_data()
+shampooList = shampooFile.get_data()
 
 for line in shampooList:
     print(line)
