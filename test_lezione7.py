f4e9363b773bff19fde051ee7eeefae4a3fc0853
Davide Gozzi <94189785+DavideGozzi02@users.noreply.github.com>
2021-12-03 16:39:43 +0000

    test

---
+++ test_lezione7.py
@@ -0,0 +1,11 @@
+import unittest
+from lezione7 import CSVFile
+
+#testing
+class TestCSVFile(unittest.TestCase):
+
+    def test_init(self):
+        csv_file = CSVFile('shampoo_sales.csv')
+        # Controllo che il nome del file sia stato salvato
+        # in un attributo dell’oggetto di nome “name”
+        self.assertEqual(csv_file.name, 'shampoo_sales.csv')
